c72807f54e209dbe2697db5648f6d13d22b4ef2d
wyz <wuyizhe@fang.com>
2019-08-21 17:01:10 +0800

    DAO接口编写

--- src/main/java/com/imooc/springboot_mybatis/dao/AreaDao.java
+++ src/main/java/com/imooc/springboot_mybatis/dao/AreaDao.java
@@ -1,4 +1,32 @@
 package com.imooc.springboot_mybatis.dao;
 
+import com.imooc.springboot_mybatis.entity.Area;
+
+import java.util.List;
+
 public interface AreaDao {
+
+    /**
+     *查询区域列表
+     */
+    List<Area> queryArea();
+    /**
+     *根据id查询区域
+     */
+    Area queryAreaById(int areaId);
+
+    /**
+     *插入区域信息
+     */
+    int insertArea(Area area);
+
+    /**
+     *更新区域信息
+     */
+    int updateArea(Area area);
+
+    /**
+     *删除区域信息
+     */
+    int deleteArea(int areaId);
 }
